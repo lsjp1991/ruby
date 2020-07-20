6eab0e86e2d83bade06092d2100ad661aa7b09c3
LJC <63648578+lsjp1991@users.noreply.github.com>
2020-07-20 14:24:51 +0200

    Add files via upload

---
+++ computer_instances_class.rb
@@ -0,0 +1,23 @@
+#Computer class that generates virtual computer instances.
+
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @files = {}
+    @@users[username] = password
+  end
+
+  def Computer.get_users
+  return @@users
+  end
+  
+  def create(filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created at #{time} by #{@username}. "
+  end
+end
+
+my_computer = Computer.new("ljc1991","Bapzuy878")
